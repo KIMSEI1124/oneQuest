62b867645cb553cb421c1ecbfca3302f90f6af64
KIMSEI <bbomi1973@gmail.com>
2022-05-25 22:16:34 +0900

    Test UserSerivceTest Create, UserRepository findAll Method Create

---  record.md
+++  record.md
@@ -1,15 +1,18 @@
 ##[ 2022 - 05 - 25 ]  
 ### Entity  
 User 생성  
-User_Community 생성 -> UserCommunity 이름 변경   
+User_Community : 생성 -> UserCommunity 이름 변경   
 
 ### Repository
-UserRepository - save 생성  
+UserRepository : save 생성, findByIdList, findAll 생성
 
 ### Service  
-UserService - id 검증, 중복 회원가입 검증, 회원가입 생성  
+UserService : id 검증, 중복 회원가입 검증, 회원가입 생성  
 
 ### Exception
-IdException 생성    
+IdException : 생성    
+
+### Test
+UserServiceTest : 생성
 
 --- 
--- src/main/java/com/oneQuest/oneQuest/repository/userRepository/UserRepository.java
+++ src/main/java/com/oneQuest/oneQuest/repository/userRepository/UserRepository.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
@@ -25,7 +26,20 @@ public class UserRepository {
     /**
      * <p>Create Date : [ 2022 - 05 - 25 ]</p>
      * <p>Last Update Date : [ 2022 - 05 - 25 ]</p>
+     * <p>전체 유저의 정보를 리스트로 가져옴</p>
+     *
+     * @return 전체 유저의 데이터
+     */
+    public List<User> findAll() {
+        return em.createQuery("select U from  User as U", User.class)
+                .getResultList();
+    }
+
+    /**
+     * <p>Create Date : [ 2022 - 05 - 25 ]</p>
+     * <p>Last Update Date : [ 2022 - 05 - 25 ]</p>
      * 유저의 id로 기존에 가입한 유저를 조회한다.
+     *
      * @param id 유저의 id
      * @return 유저의 정보를 담은 리스트 (size = 0 or 1)
      */
