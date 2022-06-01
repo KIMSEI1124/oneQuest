617c9514fb71a0676c0dfb9e53023dc2924f21f6
KIMSEI <bbomi1973@gmail.com>
2022-06-01 16:55:35 +0900

    UserCommunityRepository findByIdx Create

--- src/main/java/com/oneQuest/oneQuest/repository/userRepository/README.md
+++ src/main/java/com/oneQuest/oneQuest/repository/userRepository/README.md
@@ -1,4 +1,7 @@
 # Repository
 
 [ 2022 - 05 - 25 ]  
-UserRepository : save 생성, findByIdList, findAll 생성
+UserRepository : save 생성, findByIdList, findAll 생성  
+---
+[ 2022 - 06 - 01 ]  
+UserCommunityRepository : findByIdx 생성
--- src/main/java/com/oneQuest/oneQuest/repository/userRepository/UserCommunityRepository.java
+++ src/main/java/com/oneQuest/oneQuest/repository/userRepository/UserCommunityRepository.java
@@ -1,10 +1,12 @@
 package com.oneQuest.oneQuest.repository.userRepository;
 
+import com.oneQuest.oneQuest.domain.user.User;
 import com.oneQuest.oneQuest.domain.user.UserCommunity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -16,10 +18,22 @@ public class UserCommunityRepository {
      * <p>[ Update Date : 2022 - 05 - 30 ]</p>
      * <p>UserCommunity를 저장한다.</p>
      * @param userCommunity
-     * @return
+     * @return PK
      */
     public Long save(UserCommunity userCommunity) {
         em.persist(userCommunity);
         return userCommunity.getIdx();
     }
+
+    /**
+     * <p>[ Create Date : 2022 - 06 - 01 ]</p>
+     * <p>[ Update Date : 2022 - 06 - 01 ]</p>
+     * UserCommunity_Idx 로 유저의 관계 불러오기
+     * @param idx PK
+     * @return  UserCommunity 데이터
+     */
+    public UserCommunity findByIdx(Long idx) {
+        return em.find(UserCommunity.class, idx);
+    }
+
 }
